62c557af28bdde37cbb34d8d808b437e37ba8a1c
surynash-pushpkar <suryansh.pushpkar@webkorps.com>
2026-03-27 02:36:30 +0530

    FriendBook

---
+++ friendbook/src/main/java/com/friendbook/FriendbookApplication.java
@@ -0,0 +1,13 @@
+package com.friendbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendbookApplication.class, args);
+    }
+
+}
